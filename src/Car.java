ec91e76bb4525295122443c385f4a1b9a33e5e93
Антон <antonolegovich2017@icloud.com>
2022-09-28 00:02:11 +0600

    Domashka 3 zadanie

--- src/Car.java
+++ src/Car.java
@@ -6,25 +6,41 @@ public class Car {
     int productionYear;
     String assemblyСountry;
 
-    Car( String brand, String model,int productionYear, String assemblyСountry,String bodyColor,double engineVolume){
-        this.brand=brand;
-        this.model=model;
-        if(productionYear>=0) {
-            this.productionYear = productionYear;
+    Car(String brand, String model, int productionYear, String assemblyСountry, String bodyColor, double engineVolume) {
+        if (brand == null) {
+            this.brand = " Default";
         } else {
-            this.productionYear= Math.abs(productionYear);
+            this.brand = brand;
         }
-        this.assemblyСountry=assemblyСountry;
-        this.bodyColor=bodyColor;
-        if (engineVolume>=0) {
-            this.engineVolume = engineVolume;
-        }else {
-            this.engineVolume=Math.abs(engineVolume);
+        if (model == null) {
+            this.model = " Default";
+        } else {
+            this.model = model;
+        }
+        if (productionYear == 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = Math.abs(productionYear);
+        }
+        if (assemblyСountry == null) {
+            this.assemblyСountry = " Default";
+        } else {
+            this.assemblyСountry = assemblyСountry;
+        }
+        if (bodyColor == null) {
+            this.bodyColor = " Белый";
+        } else {
+            this.bodyColor = bodyColor;
+        }
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = Math.abs(engineVolume);
         }
     }
 
-   public String toString(){
-        return " " +brand+model+" , " +productionYear+" год выпуска, "+ " сборка в "+assemblyСountry+" , "+ bodyColor
-                +" цвет кузова, "+" объем двигателя - "+ engineVolume+".";
-   }
+    public String toString() {
+        return " " + brand + model + " , " + productionYear + " год выпуска, " + " сборка в " + assemblyСountry + " , " + bodyColor
+                + " цвет кузова, " + " объем двигателя - " + engineVolume + ".";
+    }
 }
