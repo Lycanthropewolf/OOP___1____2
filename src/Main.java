044a628f49d5295d541f67d72df801e609674f28
Антон <antonolegovich2017@icloud.com>
2022-10-02 21:02:20 +0600

    Domashka 3 zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+import transport.Car;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
 
-        Car car1 = new Car("Lada", "Granta", 2015, "России", "жёлтый", 1.7);
-        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германии", "черный", 3.0);
-        Car car3 = new Car("BMV", "Z8", 2021, "Германии", "черный", 3.0);
-        Car car4 = new Car("Kia", "Sportage 4 поколение", 2018, "Южной корее", "красный", 2.4);
-        Car car5 = new Car("Hyundai", "Avante", 2016, "Южной корее", "оранжевый", 1.6);
+        Car car1 = new Car("Lada", "Granta", 2015, "России", "жёлтый", 1.7," механическая"," седан","G345JK657",4," летняя");
+        Car car2 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германии", "черный", 3.0," автоматическая", " паркетник","34534g4",5," летняя");
+        Car car3 = new Car("BMV", "Z8", 2021, "Германии", "черный", 3.0," автоматическая", " кабриолет", "р565ол876",2," летняя");
+        Car car4 = new Car("Kia", "Sportage 4 поколение", 2018, "Южной корее", "красный", 2.4, " робот", " паркетник", "gfwefew",5," летняя");
+        Car car5 = new Car("Hyundai", "Avante", 2016, "Южной корее", "оранжевый", 1.6," автоматическая", " седан", " п544ор565", 5, " летняя");
         System.out.println(car1.toString());
         System.out.println(car2.toString());
         System.out.println(car3.toString());
---
+++ src/transport/Car.java
@@ -0,0 +1,235 @@
+package transport;
+
+import java.security.Key;
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Car {
+    private String brand;
+    private String model;
+    double engineVolume;
+    String bodyColor;
+    private int productionYear;
+    private String assemblyСountry;
+    String transmission;
+    private String bodyType;
+    String registrationNumber;
+    private int numberOfSeats;
+    String rubber;
+
+    public String getModel() {
+        if (model == null || model.isBlank() || model.isEmpty()) {
+            model = " Default";
+        } else {
+            this.model = model;
+        }
+        return model;
+    }
+
+    public String getBrand() {
+        if (brand == null || brand.isEmpty() || brand.isBlank()) {
+            brand = " Default";
+        } else {
+            this.brand = brand;
+        }
+        return brand;
+    }
+
+    public int getProductionYear() {
+        if (productionYear <= 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+        return productionYear;
+    }
+
+    public String getAssemblyСountry() {
+        if (assemblyСountry == null || assemblyСountry.isBlank() || assemblyСountry.isEmpty()) {
+            assemblyСountry = " Default";
+        } else {
+            this.assemblyСountry = assemblyСountry;
+        }
+        return assemblyСountry;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public double getEngineVolume() {
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        return engineVolume;
+    }
+
+    public String getBodyColor() {
+        if (bodyColor == null || bodyColor.isEmpty() || bodyColor.isBlank()) {
+            this.bodyColor = " белый";
+        } else {
+            this.bodyColor = bodyColor;
+        }
+        return bodyColor;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getRegistrationNumber() {
+        if (registrationNumber.matches("[A-Z]\\d\\d\\d[A-Z][A-Z]\\d\\d\\d")) {
+            this.registrationNumber = registrationNumber;
+        } else {
+            this.registrationNumber = " номер не корректен";
+        }
+        return registrationNumber;
+    }
+
+    public String getRubber() {
+        if (rubber == null || rubber.isEmpty() || rubber.isBlank()) {
+            rubber = " сменить шины на сезонные";
+        } else {
+            this.rubber = rubber;
+        }
+        return rubber;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setBodyColor(String bodyColor) {
+        this.bodyColor = bodyColor;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setRubber(String rubber) {
+        this.rubber = rubber;
+    }
+
+    public Car(String brand, String model, int productionYear, String assemblyСountry, String bodyColor, double engineVolume,
+               String transmission, String bodyType, String registrationNumber, int numberOfSeats, String rubber) {
+
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = productionYear;
+        this.assemblyСountry = assemblyСountry;
+        this.bodyColor = bodyColor;
+        this.engineVolume = Math.abs(engineVolume);
+        this.transmission = transmission;
+        this.bodyType = bodyType;
+        this.registrationNumber = registrationNumber;
+        this.numberOfSeats = numberOfSeats;
+        this.rubber = rubber;
+        this.key=getKey();
+        this.insurance=getInsurance();
+    }
+
+
+    public String toString() {
+        return " " + getBrand() + getModel() + " , " + getProductionYear() + " год выпуска, " + " сборка в " + getAssemblyСountry() + " , " + getBodyColor()
+                + " цвет кузова, " + " объем двигателя - " + getEngineVolume() + " , коробка передач" + getTransmission() + " , тип кузова - " + getBodyType() + " ,регистрационный номер -" +
+                getRegistrationNumber() + " , количество мест - " + getNumberOfSeats() + " ,резина " + getRubber();
+    }
+
+
+    private Key key;
+
+    public Key getKey() {
+
+        return key;
+    }
+
+    public Insurance getInsurance() {
+        return insurance;
+    }
+
+    private Insurance insurance;
+
+
+    public static class Key {
+
+        private String remoteEngineStart;
+        private String keylessEntry;
+
+
+        public Key(String remoteEngineStart, String keylessEntry) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessEntry = keylessEntry;
+        }
+
+        public String getRemoteEngineStart() {
+            if (remoteEngineStart == null || remoteEngineStart.isEmpty() || remoteEngineStart.isBlank()) {
+                this.remoteEngineStart = " некорректные данные";
+            } else {
+                this.remoteEngineStart = remoteEngineStart;
+            }
+            return remoteEngineStart;
+        }
+
+        public String getKeylessEntry() {
+            if (keylessEntry == null || keylessEntry.isBlank() || keylessEntry.isEmpty()) {
+                this.keylessEntry = " некорректные данные";
+            } else {
+                this.keylessEntry = keylessEntry;
+            }
+            return keylessEntry;
+        }
+
+
+    }
+
+    public static class Insurance {
+        private LocalDate durationOfInsurance;
+        private double insuranceCost;
+        private String insuranceNumber;
+
+
+        public Insurance(LocalDate durationOfInsurance, double insuranceCost, String insuranceNumber) {
+            this.durationOfInsurance = durationOfInsurance;
+            this.insuranceCost = insuranceCost;
+            this.insuranceNumber = insuranceNumber;
+        }
+
+        public void  getDurationOfInsurance() {
+            if (durationOfInsurance.isBefore(LocalDate.now())) {
+                System.out.println("Срочно оформите страховку");
+
+            } else {
+                this.durationOfInsurance=durationOfInsurance;
+            }
+        }
+
+
+        public double getInsuranceCost() {
+            return insuranceCost;
+        }
+
+        public String getInsuranceNumber() {
+            if (insuranceNumber.length()!=9){
+                System.out.println(" номер страховки некорректен");
+            }else {
+                this.insuranceNumber=insuranceNumber;
+            }
+            return insuranceNumber;
+        }
+
+
+    }
+
+}
